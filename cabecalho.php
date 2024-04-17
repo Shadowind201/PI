420e84d64ddcd15ce3f5438a3ff7e6417fdf20f7
Shadowind201 <enzocarnier@gmail.com>
2024-04-16 22:21:48 -0300

    Initial commit

---
+++ cabecalho.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Musicas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <div class="conteiner">
+        <header>
+                <a href="home.php">
+                    <i class="bi bi-house text-light m-4" style="font-size: 3rem;"></i></a>
+                <span>
+                    <a href="nova_musica.php">
+                        <i class="bi bi-pencil text-light m-4" style="font-size: 2.5rem;"></i>
+                    </a>
+                </span>
+        </header>
+
