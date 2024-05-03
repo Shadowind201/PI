845178fd906702f6f2909a1a1e49825879ff3914
Shadowind201 <enzocarnier@gmail.com>
2024-05-02 21:04:49 -0300

    .

--- cabecalho.php
+++ cabecalho.php
@@ -16,7 +16,7 @@
                     <i class="bi bi-house text-light m-4" style="font-size: 3rem;"></i></a>
                 <span>
                     <a href="nova_musica.php">
-                        <i class="bi bi-pencil text-light m-4" style="font-size: 2.5rem;"></i>
+                        <i class="bi bi-controller text-light m-4" style="font-size: 2.5rem;"></i>
                     </a>
                 </span>
         </header>
--- home.php
+++ home.php
@@ -1,5 +1,8 @@
 <?php include "cabecalho.php" ?>
 
+    <h1>Why play this game</h1>
+    <p>Nosso objetivo é fazer com que pessoas esperimentem diferentes jogos, mostrando suas caracteristicas e seu diferencial fazendo com que diferentes pessoas possam ter experiancias diferentes do que normalmente teriam com seu gosto pessoal.</p>
 
+    
 
 <?php include "rodape.php" ?>
